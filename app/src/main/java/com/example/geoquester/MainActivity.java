bf81de3d0f96434626a2d48c36e718db3774bb7a
Vadim <bgb996@mail.ru>
2019-05-07 17:51:10 +0500

    upload homework

--- app/src/main/java/com/example/geoquester/MainActivity.java
+++ app/src/main/java/com/example/geoquester/MainActivity.java
@@ -25,8 +25,20 @@ public class MainActivity extends AppCompatActivity {
     private final Logger logger = Logger.getLogger(MainActivity.class.getName());
     private Handler logHandler;
     private final Date dat = new Date();
-    private AutoCompleteTextView cityTextView;
 
+    private AutoCompleteTextView cityTextView;
+    private Button searchButton;
+
+    private CheckBox temperature;
+    private CheckBox fellingTemperature;
+    private CheckBox pressure;
+    private CheckBox humidity;
+    private CheckBox rainfall;
+    private CheckBox windSpeed;
+    private CheckBox windDirection;
+
+
+    //region LifeCycle
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,37 +46,9 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(getApplicationContext(), " - onCreate()", Toast.LENGTH_SHORT).show();
 
-        logger.setLevel(Level.ALL);
-        logger.setUseParentHandlers(false);
-        logHandler = new ConsoleHandler();
-        logHandler.setLevel(Level.ALL);
-        logHandler.setFormatter(new Formatter() {
-            @Override
-            public String format(LogRecord record) {
-                dat.setTime(record.getMillis());
-                return dat + "\n ----- "
-                        + record.getMessage() + "\n";
-            }
-        });
-
-        logger.addHandler(logHandler);
-
-        cityTextView = findViewById(R.id.cityTextView);
-        Button searchButton = findViewById(R.id.search_button);
-
-        final CheckBox temperature = findViewById(R.id.temperature);
-        final CheckBox fellingTemperature = findViewById(R.id.fellingTemperature);
-        final CheckBox pressure = findViewById(R.id.pressure);
-        final CheckBox humidity = findViewById(R.id.humidity);
-        final CheckBox rainfall = findViewById(R.id.rainfall);
-        final CheckBox windSpeed = findViewById(R.id.windSpeed);
-        final CheckBox windDirection = findViewById(R.id.windDirection);
-
-        String[] cities = getResources().getStringArray(R.array.cities);
-        List<String> catList = Arrays.asList(cities);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(
-                this, android.R.layout.simple_dropdown_item_1line, catList);
-        cityTextView.setAdapter(adapter);
+        addLogger();
+        initialization();
+        setAdapter();
 
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,11 +66,11 @@ public class MainActivity extends AppCompatActivity {
                 if (windDirection.isChecked()) parcel.setWindDirection(true);
 
                 intent.putExtra("CITY", parcel);
-                cityTextView.setText(R.string.rainfall);
                 startActivity(intent);
             }
         });
     }
+
 
     @Override
     protected void onStart() {
@@ -137,10 +121,51 @@ public class MainActivity extends AppCompatActivity {
         super.onDestroy();
         currentInfo("onDestroy()");
     }
+    //endregion
+
 
     protected void currentInfo(String loggerText){
         Toast.makeText(getApplicationContext(), loggerText, Toast.LENGTH_SHORT).show();
         logger.log(Level.INFO, loggerText);
     }
 
+    //region Preparing activity
+    private void addLogger() {
+        logger.setLevel(Level.ALL);
+        logger.setUseParentHandlers(false);
+        logHandler = new ConsoleHandler();
+        logHandler.setLevel(Level.ALL);
+        logHandler.setFormatter(new Formatter() {
+            @Override
+            public String format(LogRecord record) {
+                dat.setTime(record.getMillis());
+                return dat + "\n ----- "
+                        + record.getMessage() + "\n";
+            }
+        });
+
+        logger.addHandler(logHandler);
+    }
+
+    private void initialization (){
+        cityTextView = findViewById(R.id.cityTextView);
+        searchButton = findViewById(R.id.search_button);
+
+        temperature = findViewById(R.id.temperature);
+        fellingTemperature = findViewById(R.id.fellingTemperature);
+        pressure = findViewById(R.id.pressure);
+        humidity = findViewById(R.id.humidity);
+        rainfall = findViewById(R.id.rainfall);
+        windSpeed = findViewById(R.id.windSpeed);
+        windDirection = findViewById(R.id.windDirection);
+    }
+
+    private void setAdapter() {
+        String[] cities = getResources().getStringArray(R.array.cities);
+        List<String> catList = Arrays.asList(cities);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_dropdown_item_1line, catList);
+        cityTextView.setAdapter(adapter);
+    }
+    //endregion
 }
